f547921c6a10ccf5144ed6af3d59f3f2e0035448
Abdalla2307 <abdalla2307@users.noreply.github.com>
2026-06-22 22:46:18 +0300

    Allow start.sh to run custom commands for one-off dynos

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
-source .venv/bin/activate && python3 update.py && python3 -m bot
+if [ $# -gt 0 ]; then
+    exec "$@"
+else
+    source .venv/bin/activate && python3 update.py && python3 -m bot
+fi
+
